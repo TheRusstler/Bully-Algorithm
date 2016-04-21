0f864d641d10dc4ca0d6603bd786aabb253a5b8e
James Russell <james.russell@conduce.net>
2016-04-22 00:03:13 +0100

    crash script updated

--- crash_test.sh
+++ crash_test.sh
@@ -3,3 +3,5 @@ rm Node_*.txt
 java -jar bully.jar 3 5002 Normal 3000 config1.txt &
 java -jar bully.jar 2 5001 Crash 3000 config1.txt &
 java -jar bully.jar 1 5000 Normal 3000 config1.txt Initiator &
+sleep 10
+cat Node_*.txt >> crash_results.txt
